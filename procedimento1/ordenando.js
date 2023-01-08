cad79cf8b761d8389cb31c22ed48c8cd92b4a0ea
MariLFO <mari.fer.onorio@gmail.com>
2023-01-07 22:48:26 -0800

    Início Procedimento 1

---
+++ procedimento1/ordenando.js
@@ -0,0 +1,36 @@
+swap = (vetor, posicao1, posicao2) => {
+    // ...
+}
+
+shuffle = (vetor, trocas) => {
+    //...
+}
+
+bubble_sort = (vetor) => {
+    //...
+}
+
+selection_sort = (vetor) => {
+    //...
+} 
+
+quick_sort = (vetor, posicaoInicial, posicaoFinal) => {
+    //...
+}
+
+particionamento = (vetor, posicaoInicial, posicaoFinal, valorPivot) => {
+    //...
+}
+
+function add () {
+   
+}
+
+function ordenar () {
+
+}
+
+function misturar () {
+
+}
+
