03d940a809ff899ece2e7c933a9c777f454ee100
MariLFO <mari.fer.onorio@gmail.com>
2023-01-14 19:04:50 -0800

    Inicio procedimento 2

---
+++ procedimento2/receitas.js
@@ -0,0 +1,19 @@
+var receitas = [{
+    titulo: "Arroz de Couve-Flor",
+    imagem: "imagens/arroz-de-couve-flor.png",
+    ingredientes: ["Arroz", "Couve-Flor", "Cebola Média", "Azeite"],
+    preparo: "Deixe a couve-flor picada. Adicione os ingredientes e refogue bem. Adicione sal, tampe a panela e deixe cozinhar."
+}];
+
+function getListaIngredientes(receita) {
+
+}
+
+function getCard(receita) {
+
+}
+
+function preencheCatalog() {
+
+}
+
