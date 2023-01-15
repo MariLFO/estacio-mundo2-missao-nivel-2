3617b609a952c8c0fdeac4b8b9dc245f0c03cc4e
MariLFO <mari.fer.onorio@gmail.com>
2023-01-15 11:43:41 -0800

    Conclusão procedimento 3

--- procedimento3/usuarios.js
+++ procedimento3/usuarios.js
@@ -1,14 +1,19 @@
-$.ajax({
-    url: "https://reqres.in/api/users?per_page=10",
-    type: "GET",
-    success: function(response){
-        console.log(response.data);
-        Vue.createApp({
-            data() {
-                return {
-                    users: response.data
-                }
-            },
-        }).mount("#usuarios");
+let vue = Vue.createApp({
+    data() {
+        return {
+            users: []
+        }
+    },
+    async created() {
+        this.users = await loadUsers();
     }
-});
+}).mount("#usuarios");
+
+async function loadUsers()
+{
+    var response = await $.ajax({
+        url: "https://reqres.in/api/users?per_page=10",
+        type: "GET"
+    });
+    return response.data;
+}
