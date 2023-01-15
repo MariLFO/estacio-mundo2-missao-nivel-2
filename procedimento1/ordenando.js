4ef675fa5fb941b27a1632908a3e14e445da9d39
MariLFO <mari.fer.onorio@gmail.com>
2023-01-14 18:11:19 -0800

    Implementação do Bubble sort

--- procedimento1/ordenando.js
+++ procedimento1/ordenando.js
@@ -1,5 +1,7 @@
 swap = (vetor, posicao1, posicao2) => {
-    // ...
+    let temp = vetor[posicao1]; 
+    vetor[posicao1] = vetor[posicao2];
+    vetor[posicao2] = temp;
 }
 
 shuffle = (vetor, trocas) => {
@@ -7,7 +9,21 @@ shuffle = (vetor, trocas) => {
 }
 
 bubble_sort = (vetor) => {
-    //...
+    let troca = false;
+    for(var i = 0; i <= vetor.length-1; i++){
+        troca = false;
+        for(var j = 0; j < ( vetor.length - i -1); j++){
+            // Compara os dois números adjacentes e verifica se
+            // o primeiro é maior que o segundo
+            if(vetor[j] > vetor[j+1]){
+                // Troca eles se a condição for verdadeira
+                swap(vetor, j, j+1);
+                troca = true;
+            }
+        }
+
+        if(troca == false) return;
+    }
 }
 
 selection_sort = (vetor) => {
@@ -32,7 +48,17 @@ function add () {
 }
 
 function ordenar () {
-    alert("apertei ordenar");
+    var algoritmo = document.getElementById("algoritmo").value;
+    var valores = document.getElementById("valores");
+    var vetorValores = Array.from(valores.children).map(e => parseInt(e.innerHTML));
+    eval(algoritmo + "(vetorValores)");
+    valores.innerHTML = "";
+    vetorValores.map(i => {
+        var node = document.createElement("li");
+        var texto = document.createTextNode(i);
+        node.appendChild(texto);
+        valores.appendChild(node);
+    });
 }
 
 function misturar () {
